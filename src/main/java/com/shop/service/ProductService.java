b73aaf323b8c4bb9e40c3183b7b3228e2c6b4413
cipriantarlev <45711635+cipriantarlev@users.noreply.github.com>
2019-06-20 10:49:05 +0300

    new service

--- src/main/java/com/shop/service/ProductService.java
+++ src/main/java/com/shop/service/ProductService.java
@@ -2,16 +2,30 @@ package com.shop.service;
 
 import java.util.List;
 
+import com.shop.domain.Product;
 import com.shop.domain.enums.ProductType;
 import com.shop.domain.model.ProductDTO;
 
 public interface ProductService {
 
+	List<Product> getProductByType(ProductType type);
+
+	List<Product> findAllProduct();
+
+	Product findProductById(String id);
+
+	Product updateProduct(Product product);
+
+	Product insertProduct(Product product);
+
+	void deleteProductById(String id);
+
 	void createProduct(ProductDTO product);
 
 	List<ProductDTO> getAllProducts();
 
-	List<ProductDTO> getProductByType(ProductType type);
+	List<ProductDTO> getProductsByType(ProductType type);
 
-	ProductDTO getProductById(String id);
+	ProductDTO getProductsById(String id);
+
 }
