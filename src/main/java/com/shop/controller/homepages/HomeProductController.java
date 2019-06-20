968f1344d5c770ea5032738c37a2b5c239665313
cipriantarlev <45711635+cipriantarlev@users.noreply.github.com>
2019-06-20 10:50:40 +0300

    new index

--- src/main/java/com/shop/controller/homepages/HomeProductController.java
+++ src/main/java/com/shop/controller/homepages/HomeProductController.java
@@ -1,18 +1,21 @@
 package com.shop.controller.homepages;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.shop.domain.model.ProductModel;
+import com.shop.service.ProductService;
+
 
 @Controller
 @RequestMapping(value = "product")
 public class HomeProductController {
-//
-	// @Autowired
-	// private ProductService service;
+
+	@Autowired
+	private ProductService service;
 //
 //	@GetMapping
 //	public List<Product> getProductsByType(@RequestParam("type") String type) {
@@ -24,17 +27,24 @@ public class HomeProductController {
 //		return "homeProducts";
 //	}
 
+
+//	@GetMapping("/homepage-products")
+//	public String listAllProducts(ModelMap model) {
+//		List<ProductDTO> allProducts = service.getAllProducts();
+//		model.put("products", allProducts);
+//		return "homeProducts";
+
 //	@RequestMapping(method = RequestMethod.GET)
 //	public String listAllProducts(ModelMap model) {
 //		List<ProductDTO> allProducts = service.getAllProducts();
 //		model.put("products", allProducts);
 //		return "product/index";
+
 //	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(ModelMap modelMap) {
-		ProductModel productModel = new ProductModel();
-		modelMap.put("products", productModel.findAll());
+		modelMap.put("products", service.getAllProducts());
 		return "product/index";
 	}
 }
